317d2075539ba9ea27b27fae68a48f90e8797891
Saravasha <33827803+Saravasha@users.noreply.github.com>
2022-10-28 16:25:31 +0200

    Finally done!
    yay me!

--- js/App.js
+++ js/App.js
@@ -1,8 +1,12 @@
-
+var boxNum = 0 
+var goaledBlock = 0
+var goalNum = 0
 
 function runGame() {
     
     mapRandomizer()
+    
+    
     
     document.addEventListener('keydown', playerInputManager);
 }
@@ -65,6 +69,7 @@ function playerInputManager(e) {
 
 function playerMove(direction) {
 
+    
     let dirModifierX = 0;
     let dirModifierY = 0;
     if (direction == 'up') {
@@ -104,28 +109,65 @@ function playerMove(direction) {
         return 
     } 
 
-    if (divNextPlayerPos.classList.contains('entity-block')) {
+    if (divNextPlayerPos.classList.contains('entity-block') || divNextPlayerPos.classList.contains('entity-block-goal')) {
         
         let nextTwoSquaresAheadPosX = Math.floor(playerXcoord) + (dirModifierX * 2) ;
         let nextTwoSquaresAheadPosY = Math.floor(playerYcoord) + (dirModifierY * 2);
         let divNextTwoSquaresAheadPosCoords = nextTwoSquaresAheadPosX + '-' + nextTwoSquaresAheadPosY
         let divTwoSquaresAheadPos = document.getElementById(divNextTwoSquaresAheadPosCoords);
-        
-        /* Win Condition */
-        // if (divNextPlayerPos.classList.contains("entity-block") && divTwoSquaresAheadPos.classList.contains('tile-goal')) {
 
-        //     divTwoSquaresAheadPos.classList.remove('tile-goal');
-        //     divTwoSquaresAheadPos.classList.remove('entity-block');
-        //     divTwoSquaresAheadPos.classList.add('entity-block-goal');
-        // }  
+
+     
+
+        /* Win Condition */
+        if (divNextPlayerPos.classList.contains("entity-block") && divTwoSquaresAheadPos.classList.contains('tile-goal')) {
+
+            divTwoSquaresAheadPos.classList.remove('tile-goal');
+            divTwoSquaresAheadPos.classList.add('entity-block-goal');
+            divNextPlayerPos.classList.remove('entity-block');
+            divNextPlayerPos.classList.add('entity-player');
+            divCurrPlayerPos.classList.remove('entity-player');
+        }
+
+        if (divNextPlayerPos.classList.contains("entity-block-goal") && divTwoSquaresAheadPos.classList.contains('tile-goal')) {
+
+            divTwoSquaresAheadPos.classList.remove('tile-goal');
+            divTwoSquaresAheadPos.classList.add('entity-block-goal');
+            divNextPlayerPos.classList.remove('entity-block-goal');
+            divNextPlayerPos.classList.add('tile-goal');
+            divNextPlayerPos.classList.add('entity-player');
+            divCurrPlayerPos.classList.remove('entity-player');
+        }
+
         
-        if (!divTwoSquaresAheadPos.classList.contains('entity-block') && !divTwoSquaresAheadPos.classList.contains('tile-wall')) {
+        if (divNextPlayerPos.classList.contains('entity-block') && divTwoSquaresAheadPos.classList.contains('entity-block')) {
+        
+            return
+            }
+    
+            if (divNextPlayerPos.classList.contains('entity-block-goal') && divTwoSquaresAheadPos.classList.contains('entity-block-goal')) {
+            
+                return
+                }
+            
+
+        if (divNextPlayerPos.classList.contains('entity-block') && !divTwoSquaresAheadPos.classList.contains('tile-wall')) {
 
             divTwoSquaresAheadPos.classList.add('entity-block');
             divNextPlayerPos.classList.remove('entity-block');            
             divNextPlayerPos.classList.add('entity-player');
             divCurrPlayerPos.classList.remove('entity-player');
         }
+
+        if(divNextPlayerPos.classList.contains('entity-block-goal') && !divTwoSquaresAheadPos.classList.contains('tile-wall')) {
+
+            divTwoSquaresAheadPos.classList.add('entity-block');
+            divNextPlayerPos.classList.remove('entity-block-goal');            
+            divNextPlayerPos.classList.add('tile-goal');
+            divNextPlayerPos.classList.add('entity-player');
+            divCurrPlayerPos.classList.remove('entity-player');
+        }
+       
     }
 
     else {
@@ -133,5 +175,20 @@ function playerMove(direction) {
         divNextPlayerPos.classList.add('entity-player');
         return
     }
+
+    blockNum = document.getElementsByClassName('entity-block').length
+    goalNum = document.getElementsByClassName('tile-goal').length
+    goaledBlock = document.getElementsByClassName('entity-block-goal').length
+  
+    if (goalNum == 0) {
+        win()
+    }
 }
 
+  
+
+
+function win() {
+
+    document.getElementById('game_title').innerHTML = "You win hahaha!"
+}
